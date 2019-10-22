c9f2597c565603e9c6a071ec1982af9610867272
conorbarclay <conor@honeycombcreative.com>
2019-10-22 10:36:39 -0700

    Add switch for some html5 input types that don't correctly map from gravityforms api

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -5,65 +5,80 @@ import strings from '../../utils/strings'
 
 const Input = props => {
     const regex = props.inputMaskValue
-        ? new RegExp(props.inputMaskValue)
-        : false
+      ? new RegExp(props.inputMaskValue)
+      : false
+
+    let inputType;
+    const {type} = props;
+
+    switch(type) {
+        case 'phone':
+            inputType = 'tel';
+            break;
+        case 'website':
+            inputType = 'url';
+            break;
+        default:
+            inputType = type;
+            break;
+    }
 
     return (
-        <div
+      <div
+        className={classnames(
+          props.wrapClassName,
+          props.errors && 'gravityform__field--error'
+        )}
+      >
+          <label htmlFor={props.name} className="gravityform__label">
+              {props.label}
+              {props.maxLength > 0 &&
+               maxLengthSentence(props.maxLength, props.type)}
+          </label>
+          {outputDescription(
+            props.description,
+            props.descriptionPlacement,
+            'above'
+          )}
+          <input
+            id={props.name}
+            type={inputType}
             className={classnames(
-                props.wrapClassName,
-                props.errors && 'gravityform__field--error'
+              'gravityform__field__input',
+              `gravityform__field__input__${props.type}`,
+              props.className
             )}
-        >
-            <label htmlFor={props.name} className="gravityform__label">
-                {props.label}
-                {props.maxLength > 0 &&
-                    maxLengthSentence(props.maxLength, props.type)}
-            </label>
-            {outputDescription(
-                props.description,
-                props.descriptionPlacement,
-                'above'
-            )}
-            <input
-                id={props.name}
-                type={props.type}
-                className={classnames(
-                    'gravityform__field__input',
-                    `gravityform__field__input__${props.type}`,
-                    props.className
-                )}
-                maxLength={props.maxLength > 0 ? props.maxLength : undefined}
-                name={props.name}
-                defaultValue={props.value}
-                placeholder={props.placeholder}
-                ref={props.register({
-                    required: props.required && strings.errors.required,
-                    maxlength: {
-                        value: props.maxLength > 0 && props.maxLength,
-                        message:
-                            props.maxLength > 0 &&
-                            `${strings.errors.maxChar.front}  ${
-                                props.maxLength
-                            } ${strings.errors.maxChar.back}`,
-                    },
-                    pattern: {
-                        value: regex,
-                        message: regex && strings.errors.pattern,
-                    },
-                })}
-            />
-            {outputDescription(
-                props.description,
-                props.descriptionPlacement,
-                'below'
-            )}
-            {props.errors && (
-                <div className="gravityform__error_message">
-                    {props.errors.message}
-                </div>
-            )}
-        </div>
+            maxLength={props.maxLength > 0 ? props.maxLength : undefined}
+            name={props.name}
+            defaultValue={props.value}
+            placeholder={props.placeholder}
+            ref={props.register({
+                required: props.required && strings.errors.required,
+                maxlength: {
+                    value: props.maxLength > 0 && props.maxLength,
+                    message:
+                      props.maxLength > 0 &&
+                      `${strings.errors.maxChar.front}  ${
+                        props.maxLength
+                      } ${strings.errors.maxChar.back}`,
+                },
+                pattern: {
+                    value: regex,
+                    message: regex && strings.errors.pattern,
+                },
+            })}
+          />
+          {outputDescription(
+            props.description,
+            props.descriptionPlacement,
+            'below'
+          )}
+          {props.errors && (
+            <div className="gravityform__error_message">
+                {props.errors.message}
+            </div>
+          )}
+      </div>
     )
 }
 
